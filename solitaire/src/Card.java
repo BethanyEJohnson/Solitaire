6df518dec403cdef35fb7c11b39131ed32f7e506
MatthewMoye <31222951+MatthewMoye@users.noreply.github.com>
2019-11-12 22:45:56 -0500

    Add files via upload

--- solitaire/src/Card.java
+++ solitaire/src/Card.java
@@ -1,22 +1,41 @@
+package application;
+
+import java.io.File;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Rectangle;
+
 //one card object
-public class Card {
+public class Card extends Rectangle {
+	//because card extends rectangle, getx and gety return the top left position of the card
 	String suit;
 	String rank;
-	String state;
+	boolean isFaceUp;
 	int[] origin;
 	int[] destination;
 	
 	public Card(String suit, String rank) {
 		this.suit = suit;
 		this.rank = rank;
+		attachImage(suit, rank);
+	}
+	
+	//Attach Image to card
+	public void attachImage(String su, String ra) {
+	    File card = new File("src/cards/"+ra+su+".jpg");
+	    Image c = new Image(card.toURI().toString());
+	    ImagePattern i = new ImagePattern(c);
+	    this.setFill(i);
 	}
 	
 	//state is face up or face down
-	public String getState() {
-		return state;
+	public boolean getState() {
+		return isFaceUp;
 	}
-	public void setState(String state) {
-		this.state = state;
+	public void setState(boolean isFaceUp) {
+		this.isFaceUp = isFaceUp;
 	}
 	
 	//origin is the spot the card was in before being clicked on
