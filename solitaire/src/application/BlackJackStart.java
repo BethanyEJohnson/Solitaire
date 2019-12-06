ca6a71943efec51abbcf539a460b71130b7e0a1e
MatthewMoye <31222951+MatthewMoye@users.noreply.github.com>
2019-12-05 19:35:11 -0500

    Add files via upload

--- solitaire/src/application/BlackJackStart.java
+++ solitaire/src/application/BlackJackStart.java
@@ -1,6 +1,5 @@
 package application;
 
-import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
@@ -10,51 +9,54 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
-public class BlackJackStart extends Application {
+public class BlackJackStart implements Game{
 	Pane board = new Pane();
 	Card cardEx = new Card();
+	Button hit, one, two, three, stand;
 	final int WIDTH = cardEx.WIDTH;
 	final int HEIGHT = cardEx.HEIGHT;
 	DeckOfCards dc;
 	WinStatus win;
 
-	public void startGame() {
-		launch();
-	}
-
 	public void start(Stage primaryStage) throws Exception {
 		board = new Pane();
 		board.setStyle("-fx-background-color: green");
-		int c = 0;
 		Button newGame = new Button("New Game");
-		Button hit = new Button("Hit");
-		Button one = new Button("Bet $1");
-		Button two = new Button("Bet $2");
-		Button three = new Button("Bet $3");
-		Button stand = new Button("Stand");
+		// The button to add a card to your stack
+		hit = new Button("Hit");
+		// The button to bet one dollar
+		one = new Button("Bet $1");
+		two = new Button("Bet $2");
+		three = new Button("Bet $3");
+		stand = new Button("Stand");
 		Rectangle[] boxes = new Rectangle[2];
 		hit.setLayoutX(10);
-		hit.setLayoutY(250);
+		hit.setLayoutY(400);
 		one.setLayoutX(10);
-		one.setLayoutY(300);
+		one.setLayoutY(250);
 		two.setLayoutX(10);
-		two.setLayoutY(350);
+		two.setLayoutY(300);
 		three.setLayoutX(10);
-		three.setLayoutY(400);
+		three.setLayoutY(350);
 		stand.setLayoutX(10);
 		stand.setLayoutY(450);
+		one.setVisible(false);
+		two.setVisible(false);
+		three.setVisible(false);
+		
 		// Button to start game
 		newGame.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
+				buttonVisibility();
 				initializeBox(boxes);
 				newGame.setVisible(true);
 				board.getChildren().clear();
 				board.getChildren().addAll(newGame, hit, one, two, three, stand, boxes[0], boxes[1]);
-				
+
 				// Make a deck of cards and the stacks
 				dc = DeckOfCards.getInstance();
-				
+
 			}
 		});
 
@@ -63,12 +65,25 @@ public class BlackJackStart extends Application {
 
 		// add to stage
 		primaryStage.setScene(mainScene);
-		primaryStage.setTitle("Solitaire");
+		primaryStage.setTitle("Black Jack");
 
 		// show the stage
 		primaryStage.show();
 	}
-	
+
+	// Set button visibility
+	public void buttonVisibility() {
+		if (!one.isVisible() && !two.isVisible() && !three.isVisible()) {
+			hit.setVisible(true);
+			stand.setVisible(true);
+		} else {
+			one.setVisible(true);
+			two.setVisible(true);
+			three.setVisible(true);
+		}
+	}
+
+	// Initialize dealers' box and players' box
 	public void initializeBox(Rectangle[] r) {
 		for (int i = 0; i < r.length; i++) {
 			r[i] = new Rectangle();
--- solitaire/src/application/MainMenu.java
+++ solitaire/src/application/MainMenu.java
@@ -1,10 +1,58 @@
 package application;
 
-public class MainMenu {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class MainMenu extends Application {
 	public static void main(String[] args) {
-		SolitaireStart s = new SolitaireStart();
-		//s.startGame();
-		BlackJackStart b = new BlackJackStart();
-		b.startGame();
+		launch();
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Pane board = new Pane();
+		Scene mainScene = new Scene(board, 300, 300);
+		Button blackJack = new Button("Play BlackJack");
+		blackJack.setLayoutX(120);
+		blackJack.setLayoutY(100);
+		Button solitaire = new Button("Play Solitaire");
+		solitaire.setLayoutX(120);
+		solitaire.setLayoutY(140);
+		board.getChildren().addAll(blackJack, solitaire);
+		// add to stage
+		primaryStage.setScene(mainScene);
+		primaryStage.setTitle("Main Menu");
+		// The following two are button events that let you choose the game
+		blackJack.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				BlackJackStart b = new BlackJackStart();
+				try {
+					b.start(primaryStage);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		solitaire.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				SolitaireStart s = new SolitaireStart();
+				try {
+					s.start(primaryStage);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+		// show the stage
+		primaryStage.show();
+		
 	}
 }
